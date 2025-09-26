1623e642a91826249447a98f3ff346aa55fae81c
Aditya Chavan <chavanaditya9325@gmail.com>
2025-09-26 20:06:01 +0530

    added user model for user

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,13 +1,19 @@
-import connectDB from "./db/index.js";
-
+import express from 'express';
+import connectDB from "./config/index.js";
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config({
     path:'./env'
 })
+const app = express();
+
+app.use(cors)
+app.use(express.json({ limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb'}));
 connectDB()
 .then(() => {
-    App.listen(process.env.PORT || 5000)
+    app.listen(process.env.PORT || 5000)
     console.log(`Server is running at port : ${process.env.PORT}`);
 })
 .catch((err) => {
