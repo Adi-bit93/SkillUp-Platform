e3d0dafc9e46977524e332195d712a84099d8af2
Aditya Chavan <chavanaditya9325@gmail.com>
2025-09-26 07:14:29 +0530

    Added mongoDB server

---
+++ Backend/src/server.js
@@ -0,0 +1,15 @@
+import connectDB from "./db/index.js";
+
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+.then(() => {
+    App.listen(process.env.PORT || 5000)
+    console.log(`Server is running at port : ${process.env.PORT}`);
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!!", err);
+})
